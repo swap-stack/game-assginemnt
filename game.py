dbac1cfe5683d01d2d7c57dcc82deca7ace3a78f
Swapnila Kavishwar <62277537+swap-stack@users.noreply.github.com>
2023-12-01 12:03:14 +0530

    Added some classes

---
+++ game.py
@@ -0,0 +1,13 @@
+
+
+class Game:
+    """ Manages the game logic """
+    pass
+
+class Alien:
+    """ Represents the Animal/ Alien in the game"""
+    pass
+
+class Gun:
+    """ Represents a Weapon called Gun and its associated logic"""
+    pass
